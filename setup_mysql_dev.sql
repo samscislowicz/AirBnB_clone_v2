f132ae90f12e66bc5e23d595783c8d8d4fc0befa
Sam Scislowicz <samantha.scislowicz@gmail.com>
2017-03-17 07:35:53 +0000

    changed format

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,9 +1,5 @@
---script that prepares a MySQL server for the project
 -- Creates a database called hbnb_dev_db
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
--- Creates user with a password
-CREATE USER IF NOT EXISTS 'hbnb_dev@localhost' IDENTIFIED BY 'hbnb_dev_pwd';
--- Gives the user all privileges to database
-GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev@localhost';
--- Gives select privileges to user on performance schema
-GRANT SELECT PRIVILEGES ON performance_schema.* TO 'hbnb_dev@localhost';
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
