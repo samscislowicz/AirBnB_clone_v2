889bb3365edc75c562dec12abe418aa41e78ce8d
Sam Scislowicz <samantha.scislowicz@gmail.com>
2017-03-17 07:15:15 +0000

    added .

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -4,6 +4,6 @@ CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 -- Creates user with a password
 CREATE USER IF NOT EXISTS 'hbnb_dev@localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 -- Gives the user all privileges to database
-GRANT ALL PRIVILEGES ON hbnb_dev_db . * TO 'hbnb_dev@localhost';
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev@localhost';
 -- Gives select privileges to user on performance schema
-GRANT SELECT PRIVILEGES ON performance_schema TO 'hbnb_dev@localhost';
+GRANT SELECT PRIVILEGES ON performance_schema.* TO 'hbnb_dev@localhost';
