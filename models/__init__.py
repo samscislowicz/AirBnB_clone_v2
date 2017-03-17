345b509a3cf09ccd14173f7633f22bc8466162c5
Sam Scislowicz <samantha.scislowicz@gmail.com>
2017-03-17 09:50:10 +0000

    add imports

--- models/__init__.py
+++ models/__init__.py
@@ -7,7 +7,12 @@ from models.place import Place
 from models.state import State
 from models.review import Review
 from models.engine import file_storage
+from models.engine import db_storage
+from os import getenv
 
 
-storage = file_storage.FileStorage()
+if getenv('HBNB_TYPE_STORAGE', 'fs') == 'db':
+    storage = db_storage.DBStorage()
+else:
+    storage = file_storage.FileStorage()
 storage.reload()
