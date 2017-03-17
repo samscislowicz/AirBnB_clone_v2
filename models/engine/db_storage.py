f53d144e23c90d7e47132b6db62ee3a88eb44486
Sam Scislowicz <samantha.scislowicz@gmail.com>
2017-03-17 08:19:40 +0000

    add file

---
+++ models/engine/db_storage.py
@@ -0,0 +1,78 @@
+#!/usr/bin/python3
+from models.base_model import Base
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from os import getenv
+from models.user import User
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+"""
+This is the db_storage module
+"""
+
+
+class DBStorage:
+    __engine = None
+    __session = None
+
+    def __init__(self):
+        """
+        initializes engine
+        """
+        self.__engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'.format(
+            getenv('HBNB_MYSQL_USER'),
+            getenv('HBNB_MYSQL_PWD'),
+            getenv('HBNB_MYSQL_HOST'),
+            getenv('HBNB_MYSQL_DB')))
+        self.__models_available = {"User": User,
+                                   "Amenity": Amenity, "City": City,
+                                   "Place": Place, "Review": Review,
+                                   "State": State}
+        if getenv('HBNB_MYSQL_ENV', 'not') == 'test':
+            Base.metadata.drop_all(self.__engine)
+
+    def all(self, cls=None):
+        """
+        returns a dictionary of all the class objects
+        """
+        orm_objects = {}
+        if cls:
+            for k in self.__session.query(self.__models_available[cls]):
+                orm_objects[k.__dict__['id']] = k
+        else:
+            for i in self.__models_available.values():
+                j = self.__session.query(i).all()
+                if j:
+                    for k in j:
+                        orm_objects[k.__dict__['id']] = k
+        return orm_objects
+
+    def new(self, obj):
+        """
+        adds a new obj to the session
+        """
+        self.__session.add(obj)
+
+    def save(self):
+        """
+        saves the objects fom the current session
+        """
+        self.__session.commit()
+
+    def delete(self, obj=None):
+        """
+        deletes an object from the current session
+        """
+        if obj is not None:
+            self.__session.delete(obj)
+
+    def reload(self):
+        """
+        create all tables in the database
+        """
+        Base.metadata.create_all(self.__engine)
+        Session = sessionmaker(bind=self.__engine)
+        self.__session = Session()
