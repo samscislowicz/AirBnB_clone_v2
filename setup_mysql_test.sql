a1fadfba3e1ab726bfa24d36e52b9dbcb1e6ad8f
Sam Scislowicz <samantha.scislowicz@gmail.com>
2017-03-17 07:45:09 +0000

    Changed format

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,8 +1,5 @@
 -- a script that prepares a MySQL server for the project
--- Creates a database called hbnb_test_db
-CREATE DATABASE IF NOT EXISTS 'hbnb_test_db';
--- Creates a new user called hbnb_test
-CREATE USER IF NOT EXISTS 'hbnb_test@localhost' IDENTIFIED BY 'hbnb_test_pwd';
--- Sets privileges for user
-GRANT ALL PRIVILEGES TO 'hbnb_test@localhost' ON 'hbnb_test_db';
-GRANT SELECT PRIVILEGES TO 'hbnb_test@localhost' ON 'performance_schema';
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
