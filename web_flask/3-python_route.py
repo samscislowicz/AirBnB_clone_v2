264c6c859c0bf594d28431b1e927c2f228e4c61f
Sam Scislowicz <samantha.scislowicz@gmail.com>
2017-03-18 03:57:01 +0000

    add file

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return 'Hello HBNB!'
+
+@app.route('/hbnb')
+def hello_hbnb():
+    return 'HBNB'
+
+@app.route('/c/<text>')
+def hello_c():
+    return 'C {}'.format(text.replace("_", " "))
+
+@app.route('/python/')
+@app.route('/python/<text>')
+def hello_python(text="is cool"):
+    return 'Python {}'.format(text.replace("_", " "))
+
+if __name__=="__main__":
+    app.run(host="0.0.0.0", port="5000")
