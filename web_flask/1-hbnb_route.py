6f5173d18ad770e89f9f9b1670acd5ace742bb2c
Sam Scislowicz <samantha.scislowicz@gmail.com>
2017-03-18 03:15:20 +0000

    works

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return 'Hello HBNB!'
+
+@app.route('/hbnb')
+def hello_hbnb():
+    return 'HBNB'
+
+if __name__=="__main__":
+    app.run(host="0.0.0.0", port="5000")
