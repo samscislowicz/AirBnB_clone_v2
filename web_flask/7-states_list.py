74ed20d35d070f4dc3d063f1b0d09ee0db2eda97
Sam Scislowicz <samantha.scislowicz@gmail.com>
2017-03-18 12:04:43 +0000

    removed extra code

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -17,46 +17,6 @@ from models.state import State
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello():
-    return 'Hello HBNB!'
-
-
-@app.route('/hbnb')
-def hello_hbnb():
-    return 'HBNB'
-
-
-@app.route('/c/<text>')
-def hello_c():
-    return 'C {}'.format(text.replace("_", " "))
-
-
-@app.route('/python/')
-@app.route('/python/<text>')
-def hello_python(text="is cool"):
-    return 'Python {}'.format(text.replace("_", " "))
-
-
-@app.route('/number/<int:n>')
-def number(n):
-    return '{} is a number'.format(n)
-
-
-@app.route('/number_template/<int:n>')
-def number_template(n=None):
-    return render_template('5-number.html', n=n)
-
-
-@app.route('/number_odd_or_even/<int:n>')
-def odd_or_even(n=None):
-    if n % 2 == 0:
-        j = "even"
-    else:
-        j = "odd"
-    return render_template('6-number_odd_or_even.html', n=n, j=j)
-
-
 @app.route('/states_list')
 def states():
     return render_template('7-states_list.html',
