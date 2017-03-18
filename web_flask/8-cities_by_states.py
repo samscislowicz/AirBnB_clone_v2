744ba9ce050a42448c3002c9b3f8a01a917a3276
Sam Scislowicz <samantha.scislowicz@gmail.com>
2017-03-18 12:01:33 +0000

    lots of changes

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -67,5 +67,11 @@ def states():
 def end_sesh(exception):
     storage.close()
 
+
+@app.route("/cities_by_states")
+def cities_by_states():
+    return render_template('8-cities_by_states.html',
+                           states=storage.all(State).values())
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
