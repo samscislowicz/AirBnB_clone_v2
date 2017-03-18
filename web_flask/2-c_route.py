17c5de91794cb16cd518b80a57264a077a53e194
Sam Scislowicz <samantha.scislowicz@gmail.com>
2017-03-18 07:00:14 +0000

    pep8'ed

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -2,17 +2,20 @@
 from flask import Flask
 app = Flask(__name__)
 
+
 @app.route('/')
 def hello():
     return 'Hello HBNB!'
+
 
 @app.route('/hbnb')
 def hello_hbnb():
     return 'HBNB'
 
+
 @app.route('/c/<text>')
 def hello_c():
     return 'C {}'.format(text.replace("_", " "))
 
-if __name__=="__main__":
+if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -2,26 +2,31 @@
 from flask import Flask
 app = Flask(__name__)
 
+
 @app.route('/')
 def hello():
     return 'Hello HBNB!'
+
 
 @app.route('/hbnb')
 def hello_hbnb():
     return 'HBNB'
 
+
 @app.route('/c/<text>')
 def hello_c():
     return 'C {}'.format(text.replace("_", " "))
+
 
 @app.route('/python/')
 @app.route('/python/<text>')
 def hello_python(text="is cool"):
     return 'Python {}'.format(text.replace("_", " "))
 
+
 @app.route('/number/<int:n>')
 def number(n):
     return '{} is a number'.format(n)
 
-if __name__=="__main__":
+if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
